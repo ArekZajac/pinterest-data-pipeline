27df576ffbcf6f01faf5475023d9bd2ec9e60b0c
Arek Zajac <arekzajac@outlook.com>
2023-11-26 00:19:36 +0000

    refactored data emulation code

--- data_emulation/batch_ingestion.py
+++ data_emulation/batch_ingestion.py
@@ -1,11 +1,16 @@
+import data_emulation as core
+import os
+from dotenv import load_dotenv
+from datetime import datetime
+import json
+import requests
 
-import data_emulation
-
-class BatchIngestion:
-    def __init__(self): None
-
-    def to_msk(pin_result, geo_result, user_result):
-    api_url = os.getenv('API_URL')
+class BatchIngestor:
+    def __init__(self):
+        load_dotenv()
+    
+    def to_msk(self, pin_result, geo_result, user_result):
+        api_url = os.getenv('API_URL')
 
         def json_serializer(obj):
             if isinstance(obj, datetime):
@@ -24,11 +29,14 @@ class BatchIngestion:
             response = requests.post(f"{api_url}/topics/{topic}", data=payload, headers=headers)
             return response
     
-    responses = {
-        "pin_response": post_to_topic("0ea903d23769.pin", pin_result),
-        "geo_response": post_to_topic("0ea903d23769.geo", geo_result),
-        "user_response": post_to_topic("0ea903d23769.user", user_result)
-    }
+        responses = {
+            "pin_response": post_to_topic("0ea903d23769.pin", pin_result),
+            "geo_response": post_to_topic("0ea903d23769.geo", geo_result),
+            "user_response": post_to_topic("0ea903d23769.user", user_result)
+        }
 
-    for k, v in responses.items():
-        print(f"{k}: {v}\n")
+        for k, v in responses.items():
+            print(f"{k}: {v}\n")
+
+if __name__ == "__main__":
+    core.clock("batch")
